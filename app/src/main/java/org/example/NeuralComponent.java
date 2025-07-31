84bec3ae27f0753780ee1688d57c74674b7d78c0
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-07-31 17:44:06 +0200

    Enhance documentation for NeuralComponent interface

--- app/src/main/java/org/example/NeuralComponent.java
+++ app/src/main/java/org/example/NeuralComponent.java
@@ -1,6 +1,26 @@
 package org.example;
 
+/**
+ * NeuralComponent is a functional interface that defines a contract for components
+ * in a neural network that can process inputs and produce outputs.
+ * It is used to create a uniform interface for different components like Neuron, Layer,
+ * and NeuralNetwork.
+ * 
+ * @param <I> The type of input to the neural component.
+ * @param <O> The type of output produced by the neural component.
+ * 
+ * @author Michele Cianni
+ * @version 1.0
+ */
 @FunctionalInterface
 public interface NeuralComponent<I, O> {
+    
+    /**
+     * Processes the given input and produces an output.
+     * 
+     * @param input The input to the neural component.
+     * @return The output produced by the neural component.
+     */
     O feedForward(I input);
+    
 }
